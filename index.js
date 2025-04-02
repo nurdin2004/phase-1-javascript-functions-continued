6259a676a95b783a5af4fe0131562b3c0d53c992
nurdin2004 <adiaderkisohouf@gmail.com>
2025-04-02 03:04:57 +0300

    submiting assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(wrapper = "*") {
+    return function(adjective = "special") {
+        return `You are ${wrapper}${adjective}${wrapper}!`; 
+    };
+}
